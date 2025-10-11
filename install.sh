584987b68981a05e28be8ccd948a90e0c39d2019
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2025-10-11 13:52:45 -0400

    updated

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-pacman -Ss
-yes | pacman -Sy git vim nvim wget gzip fzf tree firefox less btop pavucontrol cmus jq python3 curl tldr sqlite3
-yes | pacman -Sy wofi kitty foot dolphin waybar hyprland xorg-xwayland hyprpaper wl-clipboard
-yes | pacman -Sy ttf-jetbrains-mono-nerd
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim/
-
-bash ./sync.sh
+pacman -Sy git vim nvim wget gzip fzf tree firefox less btop pavucontrol cmus jq python3 curl tldr sqlite3
+pacman -Sy wofi kitty foot dolphin waybar hyprland xorg-xwayland hyprpaper wl-clipboard
+pacman -Sy ttf-jetbrains-mono-nerd
