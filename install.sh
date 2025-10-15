77090b49aa29f9d16d967602b573d230c0660c09
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2025-10-15 10:18:01 -0400

    rasing up

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pacman -Sy --noconfirm --needed git vim wget gzip fzf tree firefox less btop pavucontrol jq python3 curl tldr sqlite3 fastfetch openssh
+pacman -Sy --noconfirm --needed git vim wget gzip fzf tree less btop jq python3 curl tldr sqlite3 openssh man
 pacman -Sy --noconfirm --needed nvim ripgrep tree-sitter-cli
 pacman -Sy --noconfirm --needed wofi foot waybar hyprland xorg-xwayland hyprpaper wl-clipboard
 pacman -Sy --noconfirm --needed ttf-jetbrains-mono-nerd
+pacman -Sy --noconfirm --needed firefox pavucontrol fastfetch dolphin
