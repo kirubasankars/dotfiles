b62f315666f6d96d9bb4d03fa2c259f49e044ad6
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2025-11-11 20:44:43 -0500

    fzf style full for audio player

--- mp3-audio/fzf_client.sh
+++ mp3-audio/fzf_client.sh
@@ -1,1 +1,1 @@
-fzf --bind 'enter:execute-silent(bash ~/.config/mp3-audio/track.sh {}),space:execute-silent(bash ~/.config/mp3-audio/control.sh)' --listen=/tmp/mp3-audio.sock
+fzf --style=full --bind 'enter:execute-silent(bash ~/.config/mp3-audio/track.sh {}),?:execute-silent(bash ~/.config/mp3-audio/control.sh)' --listen=/tmp/mp3-audio.sock
